8638467e53265b90ec343862634ac47f0f09d205
artificialair <jaybaker964@gmail.com>
2025-03-21 10:29:15 -0400

    added fields to Coin class

--- Coin.java
+++ Coin.java
@@ -1,3 +1,18 @@
 public class Coin {
+    protected double value;
+    protected String commonName;
+    protected String frontMotto;
+    protected int manufactureYear;
+    protected String frontImage;
+    protected String backImage;
+    protected String backMotto;
+    protected String frontLabel;
+    protected String backLabel;
+    protected String valueDescription;
+    protected String riggedEdge;
+    protected String mettalurgy;
 
+    public Coin() {
+        
+    }
 }
