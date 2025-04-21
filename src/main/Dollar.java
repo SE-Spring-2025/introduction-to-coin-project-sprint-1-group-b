be9c089aa84686582167af5b64e8900e1278d82a
artificialair <jaybaker964@gmail.com>
2025-04-21 10:21:33 -0400

    formatting changes

--- src/main/Dollar.java
+++ src/main/Dollar.java
@@ -5,9 +5,10 @@ public class Dollar extends Coin {
     public Dollar() {
         this(Calendar.getInstance().get(Calendar.YEAR));
     }
-    
+
     public Dollar(int year) {
         super(
+<<<<<<< HEAD
             "Dollar", 
             DOLLAR_VALUE, 
             "IN GOD WE TRUST", 
@@ -20,9 +21,41 @@ public class Dollar extends Coin {
             true, 
             new CuproNickel(), 
             year
+=======
+                "Dollar",
+                DOLLAR_VALUE,
+                new CuproNickel()
+>>>>>>> 552fbe2 (formatting changes)
         );
         Coin.coinCounter.incrementTotalCoins();
         Coin.coinCounter.incrementDollar();
         TotalCoins.update();
     }
+<<<<<<< HEAD
 }
+=======
+
+    protected Coin imprintFront(Coin c) {
+        c.frontMotto = "IN GOD WE TRUST";
+        c.frontLabel = "LIBERTY";
+        c.frontImage = "S_Anthony";
+        return c;
+    }
+
+    protected Coin imprintBack(Coin c) {
+        if (!c.flipped) {
+            return c;
+        }
+        c.backMotto = "E PLURIBUS UNUM";
+        c.backLabel = "UNITED STATES OF AMERICA";
+        c.backImage = "Moon_Eagle";
+        c.valueDescription = "ONE DOLLAR";
+        return c;
+    }
+
+    protected Coin edge(Coin c) {
+        c.ridgedEdge = true;
+        return c;
+    }
+}
+>>>>>>> 552fbe2 (formatting changes)
--- src/main/HalfDollar.java
+++ src/main/HalfDollar.java
@@ -1,13 +1,14 @@
 import java.util.Calendar;
 
 public class HalfDollar extends Coin {
-    
+
     public HalfDollar() {
         this(Calendar.getInstance().get(Calendar.YEAR));
     }
-    
+
     public HalfDollar(int year) {
         super(
+<<<<<<< HEAD
             "HalfDollar", 
             HALFDOLLAR_VALUE, 
             "IN GOD WE TRUST", 
@@ -20,9 +21,40 @@ public class HalfDollar extends Coin {
             true, 
             new CuproNickel(), 
             year
+=======
+                "HalfDollar",
+                HALFDOLLAR_VALUE,
+                new CuproNickel()
+>>>>>>> 552fbe2 (formatting changes)
         );
         Coin.coinCounter.incrementTotalCoins();
         Coin.coinCounter.incrementHalfDollar();
         TotalCoins.update();
     }
+<<<<<<< HEAD
+=======
+
+    protected Coin imprintFront(Coin c) {
+        c.frontMotto = "IN GOD WE TRUST";
+        c.frontLabel = "LIBERTY";
+        c.frontImage = "J_Kennedy";
+        return c;
+    }
+
+    protected Coin imprintBack(Coin c) {
+        if (!c.flipped) {
+            return c;
+        }
+        c.backMotto = "E PLURIBUS UNUM";
+        c.backLabel = "UNITED STATES OF AMERICA";
+        c.backImage = "Presidential_Seal";
+        c.valueDescription = "HALF DOLLAR";
+        return c;
+    }
+
+    protected Coin edge(Coin c) {
+        c.ridgedEdge = true;
+        return c;
+    }
+>>>>>>> 552fbe2 (formatting changes)
 }
